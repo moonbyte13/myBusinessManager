ec9b7abe4d68dd288d5b859ec1167c3d32fa87dd
Michael Frayne <fraynemichael300@gmail.com>
2023-02-19 18:49:33 -0700

    working on add_role and added README

--- lib/db/view_all_emp.sql
+++ lib/db/view_all_emp.sql
@@ -5,7 +5,7 @@ SELECT
   roles.title AS title,
   department.name AS department,
   roles.salary,
-  e2.first_name AS manager
+  CONCAT(e2.first_name, ' ', e2.last_name) AS manager
 FROM employee e1
 LEFT JOIN employee e2 ON e1.manager_id = e2.id
 JOIN roles ON e1.role_id = roles.id
